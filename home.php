463d0dad840309afebbb981885ccb55ac0ca75b2
Eduardo Hubner <eduardobennerthubner@gmail.com>
2022-08-25 15:15:04 -0300

    Inicio

---
+++ home.php
@@ -0,0 +1,1 @@
+<h1 align="center">Bem-Vindo</h1>
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Commerce</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="estilos.css">
   </head>
@@ -19,8 +19,43 @@
         <div id="divnav">
             <?php include "navbar.php"; ?>
         </div>
-        <div id="divm">
-
+        <div id="div2">
+            <h3>Categorias</h3>
+            <br>
+            <nav class="navbar navbar-expand-lg" id="nav2">
+                <div class="container-fluid">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div id="div3">
+            <?php
+                if (isset($_GET['pagina'])) {
+                    $consulta = $conn->prepare("SELECT url FROM paginas where id = :id");
+                    $resultado = $consulta->execute(array('id' => $_GET['pagina']));
+                    if ($linha = $consulta->fetch()){
+                        include $linha['url'];
+                    } else {
+                        include "404.php";
+                    }
+                } else {
+                    include "home.php";
+                }
+            ?>
         </div>
     </div>
     
